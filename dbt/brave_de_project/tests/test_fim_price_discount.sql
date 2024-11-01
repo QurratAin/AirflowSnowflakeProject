f59f32f62d31083db6217306ae02414f0509afad
qurrat <qurratulain.06@gmail.com>
2024-11-01 15:24:30 -0400

    Added Dockerfile and braveDbtGag.py to run the model with cosmos successfully

--- dbt/brave_de_project/tests/test_fim_price_discount.sql
+++
@@ -1,9 +0,0 @@
--- Testing for the presence if price is greater than discounts in fact_inventory_management_qu
-SELECT
-    COUNT(*) AS invalid_rows
-FROM
-    {{ ref('fact_inventory_management_qu') }}
-WHERE
-    price < discounts
-HAVING
-    invalid_rows > 0    
